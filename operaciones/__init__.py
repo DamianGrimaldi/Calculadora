ae439fb0486274c0e23c38f7ac2cabcda0a19874
Damián <grimaldidamian001@gmail.com>
2024-07-09 13:10:56 -0300

    Preparacion de los archivos a utilizar

---
+++ operaciones/__init__.py
@@ -0,0 +1,6 @@
+from .menuOperaciones import *
+from .suma import *
+from .resta import *
+from .multiplicacion import *
+from .division import *
+from .renderizar_texto import *
---
+++ readme.md
@@ -0,0 +1,8 @@
+Se debera crear un entorno virtual. En windows:
+
+python -m venv venv
+venv\Scripts\activate
+
+insatalar las siguientes librerias:
+
+pip install pygame==2.6.0
